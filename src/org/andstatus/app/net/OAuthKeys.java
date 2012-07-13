24fff6bd12fdb524cf7c30a066a44e3d00ec94f4
yvolk <yvolk@yurivolkov.com>
2012-07-13 19:14:57 +0400

    Added keys for the "AndStatus-OpenSource" Twitter application. Now the source code may be compiled and run without any changes. See http://f-droid.org/forums/topic/andstatus-light-weight-android-twitter-etc-client/#post-2790

--- src/org/andstatus/app/net/OAuthKeys.java
+++ src/org/andstatus/app/net/OAuthKeys.java
@@ -1,13 +1,38 @@
 package org.andstatus.app.net;
 
 /**
- * In order to compile the application you either: 
- * 1. Create your own MyOAuthKeys class with your application's
- *    TWITTER_CONSUMER_KEY and TWITTER_CONSUMER_SECRET 
- * 2. Or remove "extends MyOAuthKeys" below and un-comment the values here
- *    replacing values with your own token and key.
+ * You may compile this application:
+ * 1. With application keys provided below.
+ * 2. With keys of your own Twitter application.
+ * 
+ * Instructions:
+ * 1. Leave the file as is.
+ *    Please read this information about possible problems: 
+ *    <a href="http://blog.nelhage.com/2010/09/dear-twitter/">http://blog.nelhage.com/2010/09/dear-twitter/</a>.
+ * 2. Comment first OAuthKeys declaration and decomment second declaration. 
+ *    Create your own MyOAuthKeys class with your application's
+ *    TWITTER_CONSUMER_KEY and TWITTER_CONSUMER_SECRET
+ *    
+ *    For more information please read 
+ *    <a href="https://github.com/andstatus/andstatus/wiki/Developerfaq">Developer FAQ</a>.
+ *
+ *
+ * The first declaration: keys of the "AndStatus-OpenSource" application.
  **/
-public class OAuthKeys extends MyOAuthKeys {
-    // public static final String TWITTER_CONSUMER_KEY = "Your Twitter Application Token";
-    // public static final String TWITTER_CONSUMER_SECRET = "Your Twitter Application Secret";
+public class OAuthKeys {
+    /**
+     * Consumer key
+     */
+    public static final String TWITTER_CONSUMER_KEY = "XPHj81OgjphGlN6Jb55Kmg";
+    /**
+     * Consumer secret
+     */
+    public static final String TWITTER_CONSUMER_SECRET = "o2E5AYoDQhZf9qT7ctHLGihpq2ibc5bC4iFAOHURxw";
 }
+
+/*
+ * The second declaration. Decomment and use it with your own keys put into MyOAuthKeys.java file,
+ * which should NOT be published in the public repository
+public class OAuthKeys extends MyOAuthKeys {
+}
+*/
