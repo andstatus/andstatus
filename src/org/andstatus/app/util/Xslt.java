e92308f5cd5d656c439c361b421d6240fdcfcea0
yvolk <yvolk@yurivolkov.com>
2012-12-23 20:13:08 +0400

    Copy-paste error in comments

--- src/org/andstatus/app/util/Xslt.java
+++ src/org/andstatus/app/util/Xslt.java
@@ -1,6 +1,5 @@
 /* 
  * Copyright (C) 2012 yvolk (Yuri Volkov), http://yurivolkov.com
- * Copyright (C) 2008 Torgny Bjers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
