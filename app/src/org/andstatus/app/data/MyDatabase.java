a4bf9bc149b8dad61587fe65b2c9bb3543016cec
Yuri Volkov <yvolk@yurivolkov.com>
2015-04-23 21:49:42 +0300

    Support Library updated to v.22.1

--- app/src/org/andstatus/app/data/MyDatabase.java
+++ app/src/org/andstatus/app/data/MyDatabase.java
@@ -656,7 +656,7 @@ public final class MyDatabase extends SQLiteOpenHelper  {
                 " 7, 3,'GNUsocial.no',   'https://gnusocial.no',     1, 1,    0,  0",
                 " 8, 3,'Quitter.no',     'https://quitter.no',       1, 1,    0,  0",
                 " 9, 3,'Quitter.is',     'https://quitter.is',       1, 1,    0,  0",
-                "10, 3,'Quitter.España', 'https://quitter.es',       1, 1,    0,  0"
+                "10, 3,'Quitter.Espana', 'https://quitter.es',       1, 1,    0,  0"
                 };
         for (String value : values) {
             execSQL(db, sqlIns.replace("%s", value));
