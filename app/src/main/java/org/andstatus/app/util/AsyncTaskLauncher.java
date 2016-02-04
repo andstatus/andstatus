efe872963140805c9caed2838810e65b6bc1422c
Yuri Volkov <yvolk@yurivolkov.com>
2016-02-04 22:41:03 +0300

    #281 Added logging of tasks that are currently running

--- app/src/main/java/org/andstatus/app/util/AsyncTaskLauncher.java
+++ app/src/main/java/org/andstatus/app/util/AsyncTaskLauncher.java
@@ -18,14 +18,27 @@ package org.andstatus.app.util;
 
 import android.os.AsyncTask;
 
-import java.util.Arrays;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author yvolk@yurivolkov.com
  */
 public class AsyncTaskLauncher<Params> {
+    private static final AtomicLong launchedCount = new AtomicLong();
+
+    private static class LaunchedTask {
+        final AsyncTask<?, ?, ?> task;
+        final long startedAt = System.currentTimeMillis();
+
+        private LaunchedTask(AsyncTask<?, ?, ?> task) {
+            this.task = task;
+        }
+    }
+
+    private static final Queue<LaunchedTask> launchedTasks = new ConcurrentLinkedQueue<>();
 
     public static boolean execute(Object objTag, AsyncTask<Void, ?, ?> asyncTask) {
         return execute(objTag, asyncTask, true);
@@ -36,22 +49,70 @@ public class AsyncTaskLauncher<Params> {
         return launcher.execute(objTag, asyncTask, throwOnFail, null);
     }
 
-    public boolean execute(Object objTag, AsyncTask<Params, ?, ?> asyncTask, boolean throwOnFail, Params... params) {
+    public boolean execute(Object objTag, AsyncTask<Params, ?, ?> asyncTask,
+                           boolean throwOnFail, Params... params) {
         boolean launched = false;
         try {
             asyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, params);
+            launchedTasks.add(new LaunchedTask(asyncTask));
+            launchedCount.incrementAndGet();
+            removeFinishedTasks();
             launched = true;
         } catch (RejectedExecutionException e) {
             MyLog.w(objTag, "Launching new task\n" + threadPoolInfo(), e);
             if (throwOnFail) {
-                throw new RejectedExecutionException(MyLog.objTagToString(objTag) + "; " + threadPoolInfo(), e);
+                throw new RejectedExecutionException(MyLog.objTagToString(objTag) + "; "
+                        + threadPoolInfo(), e);
             }
         }
         return launched;
     }
 
+    public static void removeFinishedTasks() {
+        for (LaunchedTask launched : launchedTasks) {
+            if (launched.task.getStatus() == AsyncTask.Status.FINISHED) {
+                launchedTasks.remove(launched);
+            }
+        }
+    }
+
     public static String threadPoolInfo() {
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) AsyncTask.THREAD_POOL_EXECUTOR;
-        return executor.toString() + ", tasks: " + Arrays.toString(executor.getQueue().toArray());
+        return AsyncTask.THREAD_POOL_EXECUTOR.toString() + " \n" + getLaunchedTasksInfo();
+    }
+
+    private static String getLaunchedTasksInfo() {
+        StringBuilder builder = new StringBuilder("Running tasks: ");
+        long pendingCount = 0;
+        long runningCount = 0;
+        long finishedCount = 0;
+        long otherCount = 0;
+        for (LaunchedTask launched : launchedTasks) {
+            switch (launched.task.getStatus()) {
+                case RUNNING:
+                    runningCount++;
+                    builder.append(RelativeTime.secondsAgo(launched.startedAt) + " sec: "
+                            + launched.task.toString() + "\n");
+                    break;
+                case PENDING:
+                    pendingCount++;
+                    break;
+                case FINISHED:
+                    finishedCount++;
+                    break;
+                default:
+                    otherCount++;
+                    break;
+            }
+        }
+        builder.append("Total launched: " + launchedCount.get());
+        builder.append("; pending: " + pendingCount);
+        builder.append("; running: " + runningCount);
+        if (finishedCount > 0) {
+            builder.append("; just finished: " + runningCount);
+        }
+        if (otherCount > 0) {
+            builder.append("; other: " + otherCount);
+        }
+        return builder.toString();
     }
 }
