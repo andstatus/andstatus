01a27d272e13ae38585a9a82ee3a2265ebbfc2c9
Yuri Volkov <yvolk@yurivolkov.com>
2016-04-27 22:10:33 +0300

    #331 One click on a Timeline text is enough to open context menu - fixed for the Conversation view

--- app/src/main/java/org/andstatus/app/msg/ConversationViewAdapter.java
+++ app/src/main/java/org/andstatus/app/msg/ConversationViewAdapter.java
@@ -199,7 +199,6 @@ public class ConversationViewAdapter extends MyBaseAdapter {
     private void showMessageBody(ConversationViewItem item, View messageView) {
         TextView body = (TextView) messageView.findViewById(R.id.message_body);
         MyUrlSpan.showText(body, item.mBody, true);
-        body.setOnClickListener(this);
     }
 
     private void showMessageDetails(ConversationViewItem item, View messageView) {
