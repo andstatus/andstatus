089684a242b5e1c35f1cf8bbd0bba0f760e108e1
Yuri Volkov <yvolk@yurivolkov.com>
2022-12-27 23:12:12 +0300

    Don't clear current secrets on "Reverify Credentials"

--- app/src/main/kotlin/org/andstatus/app/account/AccountSettingsActivity.kt
+++ app/src/main/kotlin/org/andstatus/app/account/AccountSettingsActivity.kt
@@ -736,9 +736,6 @@ class AccountSettingsActivity : MyActivity(AccountSettingsActivity::class) {
                 // Credentials are not present,
                 // so start asynchronous OAuth Authentication process
 
-                // For now let's do everything from the very beginning
-                myAccount.connection.clearClientKeys()
-
                 if (!myAccount.areClientKeysPresent()) {
                     OAuthRegisterClientTask()
                         .execute(this, Unit)
