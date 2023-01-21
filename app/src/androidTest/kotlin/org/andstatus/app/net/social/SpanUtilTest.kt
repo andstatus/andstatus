797cab5fc644028ccc1cd6296dfb80bb1981cf1a
Yuri Volkov <yvolk@yurivolkov.com>
2023-01-21 11:45:57 +0300

    Fix SpanUtilTest: Origin is not EMPTY now

--- app/src/androidTest/kotlin/org/andstatus/app/net/social/SpanUtilTest.kt
+++ app/src/androidTest/kotlin/org/andstatus/app/net/social/SpanUtilTest.kt
@@ -60,11 +60,11 @@ class SpanUtilTest {
         audience.add(ma.actor)
         val modifier = SpanUtil.spansModifier(audience)
         val text = "@<span class=\"vcard\"><a href=\"http://micro.site.com/johnsmith\" class=\"url\"" +
-                " title=\"johnsmith\"><span class=\"fn nickname mention\">johnsmith</span></a></span>" +
-                " @<span class=\"vcard\"><a href=\"http://micro.site.com/" + username2 +
-                "\" class=\"url\" title=\"" + username2 + "\">" +
-                "<span class=\"fn nickname mention\">" + username2 + "</span></a></span>" +
-                " Please apply your #logic #Логика to another subject."
+            " title=\"johnsmith\"><span class=\"fn nickname mention\">johnsmith</span></a></span>" +
+            " @<span class=\"vcard\"><a href=\"http://micro.site.com/" + username2 +
+            "\" class=\"url\" title=\"" + username2 + "\">" +
+            "<span class=\"fn nickname mention\">" + username2 + "</span></a></span>" +
+            " Please apply your #logic #Логика to another subject."
         val spannable: Spannable = MyUrlSpan.Companion.toSpannable(text, TextMediaType.HTML, true)
         val regions1 = SpanUtil.regionsOf(spannable)
         val message1 = "Regions before change: $regions1"
@@ -76,40 +76,51 @@ class SpanUtilTest {
         Assert.assertEquals(message2, 4, spans.size.toLong())
         Assert.assertEquals(message2, 6, regions2.size.toLong())
         oneMention(regions2, message2, 0, "johnsmith")
-        Assert.assertEquals(message2, "content://timeline.app.andstatus.org/note/" + ma.actor.actorId +
-                "/lt/sent/origin/0/actor/" + ma.actor.actorId,
-                regions2[1].urlSpan.get().url)
+        Assert.assertEquals(
+            message2, "content://timeline.app.andstatus.org/note/" + ma.actor.actorId +
+                "/lt/sent/origin/${ma.origin.id}/actor/" + ma.actor.actorId,
+            regions2[1].urlSpan.get().url
+        )
         oneHashTag(regions2, message2, 3, "logic")
-        oneHashTag(regions2, message2, 4, "#Логика",
-                "content://timeline.app.andstatus.org/note/0/lt/search/origin/0/actor/0/search/" +
-                        "%23%D0%9B%D0%BE%D0%B3%D0%B8%D0%BA%D0%B0")
+        oneHashTag(
+            regions2, message2, 4, "#Логика",
+            "content://timeline.app.andstatus.org/note/0/lt/search/origin/0/actor/0/search/" +
+                "%23%D0%9B%D0%BE%D0%B3%D0%B8%D0%BA%D0%B0"
+        )
     }
 
     private fun oneMention(regions: List<SpanUtil.Region>, message: String, index: Int, username: String) {
         val region = regions.get(index)
         val urlSpan = region.urlSpan
-        val actor = urlSpan.flatMap { u: MyUrlSpan -> u.data.actor }
-        val accountToSync = actor.map { a: Actor -> a.origin.myContext.accounts.toSyncThatActor(a) }
-        Assert.assertTrue("Region $index should be a mention $region\n$message", actor.isPresent)
+        val actor = urlSpan.flatMap { u: MyUrlSpan -> u.data.actor }.orElseThrow {
+            AssertionError("No actor: Region $index should be a mention $region\n$message")
+        }
+        val accountToSync = actor.origin.myContext.accounts.toSyncThatActor(actor)
         Assert.assertEquals("Region $index $message",
-                "content://timeline.app.andstatus.org/note/" +
-                        accountToSync.map { obj: MyAccount -> obj.actorId }.orElse(0L) +
-                        "/lt/sent/origin/0/actor/0",
-                urlSpan.map { obj: MyUrlSpan -> obj.getURL() }.orElse(""))
-        Assert.assertEquals("Username in region $index $message",
-                username.toUpperCase(), actor.map { obj: Actor -> obj.getUsername() }.orElse("").toUpperCase())
+            "content://timeline.app.andstatus.org/note/" +
+                accountToSync.actorId +
+                "/lt/sent/origin/${actor.origin.id}/actor/0",
+            urlSpan.map { obj: MyUrlSpan -> obj.getURL() }.orElse("")
+        )
+        Assert.assertEquals(
+            "Username in region $index $message",
+            username.toUpperCase(), actor.getUsername().toUpperCase()
+        )
     }
 
     private fun oneHashTag(regions: List<SpanUtil.Region>, message: String, index: Int, term: String) {
-        oneHashTag(regions, message, index, "#$term",
-                "content://timeline.app.andstatus.org/note/0/lt/search/origin/0/actor/0/search/%23$term")
+        oneHashTag(
+            regions, message, index, "#$term",
+            "content://timeline.app.andstatus.org/note/0/lt/search/origin/0/actor/0/search/%23$term"
+        )
     }
 
     private fun oneHashTag(regions: List<SpanUtil.Region>, message: String, index: Int, hashTag: String, url: String) {
         val region = regions.get(index)
         val urlSpan = region.urlSpan
         Assert.assertEquals("Region $index should be a hashtag $region\n$message", hashTag,
-                urlSpan.flatMap { u: MyUrlSpan -> u.data.searchQuery }.orElse(""))
+            urlSpan.flatMap { u: MyUrlSpan -> u.data.searchQuery }.orElse("")
+        )
         val timeline = urlSpan.map { u: MyUrlSpan -> u.data.getTimeline() }.orElse(Timeline.EMPTY)
         Assert.assertEquals(message, hashTag, timeline.searchQuery)
         val onClickUrl = urlSpan.map { obj: MyUrlSpan -> obj.getURL() }.orElse("")
@@ -122,13 +133,13 @@ class SpanUtilTest {
     fun manyHashtags() {
         val modifier = SpanUtil.spansModifier(Audience.Companion.EMPTY)
         val text = "As somebody said there are 5 'grand challenges' to stopping floods," +
-                " hurricanes, and drought    https://andstatus.org/somelink" +
-                " #ActOnClimate #SR15 #IPCC #1o5 #Agriculture #Electricity" +
-                " #.testnonhashtag" +  // Invalid hashtag
-                " #transportation  #buildings  #Manufacturing" +
-                " #s #1" +  // Valid testing hashtags
-                " #ßCleanEnergy #GHG" +
-                " ß #ZeroCarbon" // "ß" Upper case is "SS"
+            " hurricanes, and drought    https://andstatus.org/somelink" +
+            " #ActOnClimate #SR15 #IPCC #1o5 #Agriculture #Electricity" +
+            " #.testnonhashtag" +  // Invalid hashtag
+            " #transportation  #buildings  #Manufacturing" +
+            " #s #1" +  // Valid testing hashtags
+            " #ßCleanEnergy #GHG" +
+            " ß #ZeroCarbon" // "ß" Upper case is "SS"
         val spannable: Spannable = MyUrlSpan.Companion.toSpannable(text, TextMediaType.HTML, true)
         val regions1 = SpanUtil.regionsOf(spannable)
         val message1 = "Regions before change: $regions1"
@@ -150,9 +161,11 @@ class SpanUtilTest {
         oneHashTag(regions2, message2, 11, "Manufacturing")
         oneHashTag(regions2, message2, 12, "s")
         oneHashTag(regions2, message2, 13, "1")
-        oneHashTag(regions2, message2, 14, "#ßCleanEnergy",
-                "content://timeline.app.andstatus.org/note/0/lt/search/origin/0/actor/0/search/" +
-                        "%23%C3%9FCleanEnergy")
+        oneHashTag(
+            regions2, message2, 14, "#ßCleanEnergy",
+            "content://timeline.app.andstatus.org/note/0/lt/search/origin/0/actor/0/search/" +
+                "%23%C3%9FCleanEnergy"
+        )
         oneHashTag(regions2, message2, 15, "GHG")
         notAHashTag(regions2, message2, 16)
         oneHashTag(regions2, message2, 17, "ZeroCarbon")
@@ -162,7 +175,8 @@ class SpanUtilTest {
         val region = regions.get(index)
         val urlSpan = region.urlSpan
         Assert.assertEquals("Region $index should not be a hashtag $region\n$message", "",
-                urlSpan.flatMap { u: MyUrlSpan -> u.data.searchQuery }.orElse(""))
+            urlSpan.flatMap { u: MyUrlSpan -> u.data.searchQuery }.orElse("")
+        )
     }
 
     @Test
@@ -179,19 +193,19 @@ class SpanUtilTest {
 
     private fun textWithMentions1(modifier: Function<Spannable, Spannable>) {
         val text = "<p><span class=\"h-card\"><a href=\"https://netzkombin.at/users/nipos\" class=\"u-url mention\">" +
-                "@<span>nipos</span></a></span> Unfortunately, <a href=\"https://mastodon.social/tags/mastodon\"" +
-                " class=\"mention hashtag\" rel=\"tag\">#<span>Mastodon</span></a> API uses instance-specific" +
-                " identifiers (just like <a href=\"https://mastodon.social/tags/twitter\" class=\"mention hashtag\"" +
-                " rel=\"tag\">#<span>Twitter</span></a>, which _is_ a single instance from a user&apos;s point of view)," +
-                " which locks a user to this instance. Long story is starting from this comment:" +
-                " <a href=\"https://github.com/andstatus/andstatus/issues/419#issuecomment-254031208\"" +
-                " rel=\"nofollow noopener\" target=\"_blank\">" +
-                "<span class=\"invisible\">https://</span><span class=\"ellipsis\">github.com/andstatus/andstatus</span>" +
-                "<span class=\"invisible\">/issues/419#issuecomment-254031208</span></a><br />" +
-                "<span class=\"h-card\">" +
-                "<a href=\"https://social.mecanis.me/@er1n\" class=\"u-url mention\">@<span>er1n</span></a></span>" +
-                " <span class=\"h-card\"><a href=\"https://mastodon.at/@switchingsocial\" class=\"u-url mention\">" +
-                "@<span>switchingsocial</span></a></span></p>"
+            "@<span>nipos</span></a></span> Unfortunately, <a href=\"https://mastodon.social/tags/mastodon\"" +
+            " class=\"mention hashtag\" rel=\"tag\">#<span>Mastodon</span></a> API uses instance-specific" +
+            " identifiers (just like <a href=\"https://mastodon.social/tags/twitter\" class=\"mention hashtag\"" +
+            " rel=\"tag\">#<span>Twitter</span></a>, which _is_ a single instance from a user&apos;s point of view)," +
+            " which locks a user to this instance. Long story is starting from this comment:" +
+            " <a href=\"https://github.com/andstatus/andstatus/issues/419#issuecomment-254031208\"" +
+            " rel=\"nofollow noopener\" target=\"_blank\">" +
+            "<span class=\"invisible\">https://</span><span class=\"ellipsis\">github.com/andstatus/andstatus</span>" +
+            "<span class=\"invisible\">/issues/419#issuecomment-254031208</span></a><br />" +
+            "<span class=\"h-card\">" +
+            "<a href=\"https://social.mecanis.me/@er1n\" class=\"u-url mention\">@<span>er1n</span></a></span>" +
+            " <span class=\"h-card\"><a href=\"https://mastodon.at/@switchingsocial\" class=\"u-url mention\">" +
+            "@<span>switchingsocial</span></a></span></p>"
         val spannable: Spannable = MyUrlSpan.Companion.toSpannable(text, TextMediaType.HTML, true)
         val regions1 = SpanUtil.regionsOf(spannable)
         val message1 = "Regions before change: $regions1"
@@ -234,12 +248,12 @@ class SpanUtilTest {
         addRecipient(ma, audience, "qwertystop", "329431")
         val modifier = SpanUtil.spansModifier(audience)
         val text = "@<a href=\"https://mastodon.social/users/AndStatus\" class=\"h-card u-url p-nickname mention\" " +
-                "rel=\"nofollow noopener\" target=\"_blank\">andstatus</a> " +
-                "@<a href=\"https://wandering.shop/users/qwertystop\" class=\"h-card u-url p-nickname mention\" " +
-                "rel=\"nofollow noopener\" target=\"_blank\">qwertystop</a> Which is how it is encoded in the XML. <br>" +
-                "    <br> CW should properly have been implemented using either some new field, or using in-line stuff" +
-                " like #<span class=\"\"><a href=\"https://social.umeahackerspace.se/tag/cw\" rel=\"nofollow noopener\"" +
-                " target=\"_blank\">cw</a></span> &lt;foo&gt; on the first line."
+            "rel=\"nofollow noopener\" target=\"_blank\">andstatus</a> " +
+            "@<a href=\"https://wandering.shop/users/qwertystop\" class=\"h-card u-url p-nickname mention\" " +
+            "rel=\"nofollow noopener\" target=\"_blank\">qwertystop</a> Which is how it is encoded in the XML. <br>" +
+            "    <br> CW should properly have been implemented using either some new field, or using in-line stuff" +
+            " like #<span class=\"\"><a href=\"https://social.umeahackerspace.se/tag/cw\" rel=\"nofollow noopener\"" +
+            " target=\"_blank\">cw</a></span> &lt;foo&gt; on the first line."
         val spannable: Spannable = MyUrlSpan.Companion.toSpannable(text, TextMediaType.HTML, true)
         val regions1 = SpanUtil.regionsOf(spannable)
         val message1 = "Regions before change: $regions1"
