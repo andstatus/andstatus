3c86093af6514c0e436f965d8f921e47ab0c0e1e
Yuri Volkov <yvolk@yurivolkov.com>
2023-07-16 17:11:25 +0300

    v60.04 Fix ActivityPub newer page request

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     buildToolsVersion = rootProject.extra["buildToolsVersion"] as String
 
     defaultConfig {
-        versionCode = 363
-        versionName = "60.03"
+        versionCode = 364
+        versionName = "60.04"
 
         applicationId = "org.andstatus.app"
         minSdk = rootProject.extra["minSdk"] as Int
